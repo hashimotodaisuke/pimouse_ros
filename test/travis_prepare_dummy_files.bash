1f17ff0af6ebdd348a560e915ae5b9029ca1ef6e
Daisuke Hashimoto <hashimoto@example.com>
2017-04-17 09:18:31 +0000

    modify test/travis_prepare_dummy_files.bash

--- test/travis_prepare_dummy_files.bash
+++ test/travis_prepare_dummy_files.bash
@@ -6,7 +6,7 @@ sudo touch /dev/rt{buzzer,motor,motoren,motor_raw_{l,r}}0
 sudo chmod 666 /dev/rt{buzzer,motor,motoren,motor_raw_{l,r}}0
 # make dummy input file
 # tee command is output to 2ways(file and commandline)
-echo "0 0 0 0" | sudo teee /dev/rtlightsensor0
+echo "0 0 0 0" | sudo tee /dev/rtlightsensor0
 sudo chmod 666 /dev/rtlightsensor0
 # make dummy input file
 echo "0" | sudo tee /dev/rtswitch{0,1,2}
